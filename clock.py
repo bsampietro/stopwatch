0428dd7c436561e8fb72c2d8cdce8e30f3ed3401
Bruno Sampietro <bsampietro@gmail.com>
2020-04-12 12:23:43 -0300

    Initial commit

---
+++ clock.py
@@ -0,0 +1,43 @@
+import time
+
+class Clock:
+    def __init__(self):
+        self.reset()
+
+    def start(self):
+        if self.running:
+            return
+        self.running = True
+        self._start_time = time.time()
+
+    def stop(self):
+        if not self.running:
+            return
+        self.running = False
+        self._elapsed_time += int(time.time() - self._start_time)
+
+    def reset(self):
+        self._start_time = None
+        self._elapsed_time = 0
+        self.running = False
+
+    def elapsed_time(self):
+        if self.running:
+            return self._elapsed_time + int(time.time() - self._start_time)
+        else:
+            return self._elapsed_time
+
+    def move(self, duration):
+        self._elapsed_time += duration
+
+    def display_time(self, with_seconds):
+        elapsed_time = self.elapsed_time()
+        sign = '-' if elapsed_time < 0 else ''
+        elapsed_time = abs(elapsed_time)
+        hours = elapsed_time // 3600
+        minutes = (elapsed_time - hours * 3600) // 60
+        result = '%s%02d:%02d' % (sign, hours, minutes)
+        if with_seconds:
+            seconds = elapsed_time - (hours * 3600 + minutes * 60)
+            result += ':%02d' % seconds
+        return result
---
+++ main.py
@@ -0,0 +1,124 @@
+import tkinter as tk
+from clock import Clock
+
+# https://www.w3schools.com/charsets/ref_html_utf8.asp
+# to use UTF-8 symbols: chr uses dec representation and \u hex representation
+START_TEXT = ">" # chr(9655) # "\u25B6"
+STOP_TEXT = "O"  # chr(9633) # "\u25A0"
+CLOSE_TEXT = "X" # "\u2715"
+
+class StopWatch(tk.Frame):
+    def __init__(self, master=None):
+        super().__init__(master)
+
+        self.clock = Clock()
+        self.with_seconds = False
+
+        self.timestr = tk.StringVar()
+        self.create_widgets()
+
+    def create_widgets(self):
+        self.lbl_display = tk.Label(self, textvariable=self.timestr)
+        self.lbl_display.pack(side=tk.LEFT)
+        self.lbl_display.bind('<Double-Button-1>', self.reset)
+        self.lbl_display.bind('<Button-4>', self.add)
+        self.lbl_display.bind('<Button-5>', self.substract)
+        self.lbl_display.bind('<Button-3>', self.toggle_show_seconds)
+        # self.lbl_display.bind('<MouseWheel>', self.add) # windows
+        self.update_label()
+
+        self.btn_start_stop = tk.Button(
+            self, text=START_TEXT, padx=5, pady=0, command=self.start_stop
+        )
+        self.btn_start_stop.pack(side=tk.LEFT)
+
+        self.btn_close = tk.Button(
+            self, text=CLOSE_TEXT, padx=5, pady=0, command=quit
+        )
+        self.btn_close.pack(side=tk.LEFT)
+
+
+    ## Event handlers
+
+    def start_stop(self):
+        if self.clock.running:
+            self.clock.stop()
+            self.cancel_refresh_loop()
+            self.btn_start_stop.config(text=START_TEXT)
+        else:
+            self.clock.start()
+            self.refresh_loop()
+            self.btn_start_stop.config(text=STOP_TEXT)
+
+    def substract(self, event):
+        self.clock.move(-300)
+        self.update_label()
+
+    def add(self, event):
+        self.clock.move(300)
+        self.update_label()
+    
+    def reset(self, event):
+        if self.clock.running:
+            return
+        self.clock.reset()
+        self.update_label()
+
+    def toggle_show_seconds(self, event):
+        self.with_seconds = not self.with_seconds
+        if self.clock.running:
+            self.cancel_refresh_loop()
+            self.refresh_loop()
+        else:
+            self.update_label()
+
+
+    ## Helper methods
+
+    def refresh_loop(self):
+        self.update_label()
+        self.refresh_loop_reference = self.after(
+            self.refresh_interval(), self.refresh_loop
+        )
+
+    def cancel_refresh_loop(self):
+        if not hasattr(self, 'refresh_loop_reference'):
+            return
+        self.after_cancel(self.refresh_loop_reference)
+
+    def update_label(self):
+        self.timestr.set(self.clock.display_time(self.with_seconds))
+
+    def refresh_interval(self):
+        return 1000 if self.with_seconds else 60000 # in milisec
+
+
+def main():
+    root = tk.Tk()
+    #root.title("SW")
+    
+    ## Remove this section to create normal window
+    root.overrideredirect(True)
+    root.wm_attributes('-topmost', True)
+
+    ## Position the window in the center
+    ws = root.winfo_screenwidth() # width of the screen
+    hs = root.winfo_screenheight() # height of the screen
+    #root.geometry('+%d+%d'%(ws-170,hs-60))
+    root.geometry('+%d+%d' % (ws // 2, hs // 2))
+
+    sw = StopWatch(root)
+    sw.pack(side=tk.TOP)
+
+    ## Adds capability to move window clicking on frame
+    def moving(event):
+        x_ = event.x_root
+        y_ = event.y_root
+        root.geometry("+%s+%s" % (x_, y_))
+    sw.lbl_display.bind("<B1-Motion>", moving)
+
+    ## Mainloop
+    root.mainloop()
+
+if __name__ == '__main__':
+    main()
